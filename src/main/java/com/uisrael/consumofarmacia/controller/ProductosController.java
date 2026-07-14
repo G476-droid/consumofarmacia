ff8c161885a06a4e5ba2802d535e9b55a01138b3
Gloria Fernanda Cumbicos Canar <gloria.cumbicos.canar@estudiantes.cordillera.edu.ec>
2026-07-14 13:24:15 -0500

    feat: prueba de commit

--- src/main/java/com/uisrael/consumofarmacia/controller/ProductosController.java
+++ src/main/java/com/uisrael/consumofarmacia/controller/ProductosController.java
@@ -5,17 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/productos")
+@RequestMapping("/producto")
 public class ProductosController {
 			
 	@GetMapping
 	public String leerPagina() {
-		return "productos/listar";
+		return "/productos/listar";
 	}
 	
-	@GetMapping("/listar")
-	public String listarPagina() {
-		return "productos/listar";
-	}
+// hola
 
 }
